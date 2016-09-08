11e8cb8e19d3aabeff63ce4a4b93c3cba57cd021
Tree <Tree>
2016-09-08 23:21:22 +0800

    Initial commit

---
+++ app/src/main/java/com/example/user/test16/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.user.test16;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button bt = (Button) findViewById(R.id.button);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent("SecondActivity", Uri.parse("web://www.baidu.com"));
+                startActivity(intent);
+
+            }
+        });
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/user/test16/SecondActivity.java
@@ -0,0 +1,17 @@
+package com.example.user.test16;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by lenovo on 2016/9/1.
+ */
+public class SecondActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+
+}
